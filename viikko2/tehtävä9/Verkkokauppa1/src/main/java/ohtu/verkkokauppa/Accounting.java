0cc92855b9881d8f5658516eecd516ecd3f984f2
Elmeri Lehikoinen <elmeri.lehikoinen@gmail.com>
2020-04-22 19:48:07 +0300

    viikko2_tehtävät_9-14

---
+++ viikko2/tehtävä9/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Accounting.java
@@ -0,0 +1,12 @@
+
+package ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public interface Accounting {
+
+    ArrayList<String> getTapahtumat();
+
+    void lisaaTapahtuma(String tapahtuma);
+    
+}
