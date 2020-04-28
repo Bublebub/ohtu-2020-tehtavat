456316604166474f5a1181714624a44185d65862
Elmeri Lehikoinen <elmeri.lehikoinen@gmail.com>
2020-04-28 19:17:01 +0300

    viikko6 tehtävä2

---
+++ viikko6/tehtävä2/QueryLanguage/src/main/java/statistics/matcher/Not.java
@@ -0,0 +1,20 @@
+
+package statistics.matcher;
+
+import statistics.Player;
+
+
+public class Not implements Matcher {
+
+    Matcher matcher;
+    
+    public Not(Matcher matcher) {
+        this.matcher = matcher;
+    }
+    
+    @Override
+    public boolean matches(Player p) {
+        return !matcher.matches(p);
+    }
+    
+}
